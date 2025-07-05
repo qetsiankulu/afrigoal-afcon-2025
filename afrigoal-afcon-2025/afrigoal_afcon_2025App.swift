a24ac9fda9bc52a61c230969c919db04e8675400
Qetsia Nkulu <qetsiankulu@gmail.com>
2025-07-05 18:38:57 +0100

    Initial Commit

---
+++ afrigoal-afcon-2025/afrigoal_afcon_2025App.swift
@@ -0,0 +1,17 @@
+//
+//  afrigoal_afcon_2025App.swift
+//  afrigoal-afcon-2025
+//
+//  Created by Qetsia Nkulu  on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct afrigoal_afcon_2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
